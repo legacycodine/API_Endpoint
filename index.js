dff1aa59e923ea132bd40017fde91eca4974d30a
legacycodine <adebimpee842@gmail.com>
2023-09-14 09:26:26 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 // Define an endpoint that takes two query parameters
-app.get('/endpoint', (req, res) => {
+app.get('/api', (req, res) => {
   const slackName = req.query.slack_name;
   const track = req.query.track;
 
